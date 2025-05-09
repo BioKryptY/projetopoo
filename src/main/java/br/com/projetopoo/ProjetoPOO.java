eb4e6e4eb32b43c26040ecd4286cfab69736f9e9
Lucas Fernandes <lucas.fernandes@g-iatech.com>
2025-05-09 10:20:07 -0300

    Arquivos iniciais

---
+++ src/main/java/br/com/projetopoo/ProjetoPOO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.projetopoo;
+
+/**
+ *
+ * @author luqin
+ */
+public class ProjetoPOO {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new TelaInicial().setVisible(true);
+        });
+    }
+}
